69cce3838273744a1e19a065780fdfccf85459a7
HC32 <124742918+HC32@users.noreply.github.com>
2023-05-29 19:25:57 -0400

    Set up results and fix referencing
    Add the function to play a round and declare the results.
    Fixed the html file reference to js file.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
+let array = ["rock", "paper", "scissors"]
+
 function getComputerChoice() {
     return array[Math.floor(Math.random() * array.length)]
 }
 
 let computerSelection = getComputerChoice()
 
-let playerSelection = prompt("Do you want to throw rock, paper, or scissors?")
+let playerSelection = getPlayerChoice()
+
+function getPlayerChoice() {
+    return prompt("Do you want to throw rock, paper, or scissors?")
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+            alert("You win!")
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+            alert("You win!")
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+            alert("You win!")
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+            alert("You lose!")
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+            alert("You lose!")
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+            alert("You lose!")
+    } else if (playerSelection.toLowerCase() === computerSelection) {
+            alert("It's a draw!")
+    } else {
+            alert("Please choose rock, paper, or scissors.")
+    }
+}

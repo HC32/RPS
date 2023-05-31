8c2f567959b780bdb3fd4e25250cb685cc11adae
HC32 <124742918+HC32@users.noreply.github.com>
2023-05-31 19:06:27 -0400

    Change function to console log
    Change function to console log messages so that the function does not
    need to be run with console log.

--- js/script.js
+++ js/script.js
@@ -6,27 +6,23 @@ function getComputerChoice() {
 
 let computerSelection = getComputerChoice()
 
-let playerSelection = getPlayerChoice()
+let playerSelection = prompt("Do you want to throw rock, paper, or scissors?")
 
-function getPlayerChoice() {
-    return prompt("Do you want to throw rock, paper, or scissors?")
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
-            return "You win!"
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
-            return "You win!"
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
-            return "You win!"
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
-            return "You lose!"
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
-            return "You lose!"
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-            return "You lose!"
-    } else if (playerSelection.toLowerCase() === computerSelection) {
-            return "It's a draw!"
+function playRound() {
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("Rock beats scissors! You win!")
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("Scissors beats paper! You win!")
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("Paper beats rock! You win!")
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("Paper beats rock! You lose!")
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("Scissors beats paper! You lose!")
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("Rock beats scissors! You lose!")
+    } else if (playerSelection === computerSelection) {
+        console.log(`You both picked ${playerSelection}! It's a draw!`)
     } else {
             return "Please choose rock, paper, or scissors."
     }

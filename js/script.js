30a128a5c96ad623b4b56a68aeb697b6ebf24d20
HC32 <124742918+HC32@users.noreply.github.com>
2023-07-02 13:38:55 -0400

    Add user interface: buttons, results display
    Add buttons for player selections and container to display results on
    webpage. Add click event listeners for buttons to run function to use
    the clicked button as the player's selection, generate a random choice
    for the computer's selection, and run the previously created playRound
    function to run the game. Add function to declare results within results
    container on webpage and add it to the playRound function.

--- js/script.js
+++ js/script.js
@@ -1,48 +1,74 @@
 let array = ["rock", "paper", "scissors"]
 
 function getComputerChoice() {
-    return array[Math.floor(Math.random() * array.length)]
+    let random = array[Math.floor(Math.random() * array.length)];
+    return random;
 }
-
-
 
 let playerScore = 0
 
 let computerScore = 0
 
-function playRound() {
-        let playerSelection = prompt("Do you want to throw rock, paper, or scissors?");
-        let computerSelection = getComputerChoice();
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore += 1;
-        console.log(`Rock beats scissors! You win! \n Player: ${playerScore} \n Computer: ${computerScore}`);
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore += 1;
-        console.log(`Scissors beats paper! You win! \n Player: ${playerScore} \n Computer: ${computerScore}`);
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore += 1;
-        console.log(`Paper beats rock! You win! \n Player: ${playerScore} \n Computer: ${computerScore}`);
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore += 1;
-        console.log(`Paper beats rock! You lose! \n Player: ${playerScore} \n Computer: ${computerScore}`);
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        computerScore += 1;
-        console.log(`Scissors beats paper! You lose! \n Player: ${playerScore} \n Computer: ${computerScore}`);
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore += 1;
-        console.log(`Rock beats scissors! You lose! \n Player: ${playerScore} \n Computer: ${computerScore}`);
-    } else if (playerSelection === computerSelection) {
-        console.log(`You both picked ${playerSelection}! It's a draw!  \n Player: ${playerScore} \n Computer: ${computerScore}`);
-    } else {
-        console.log(`Please choose rock, paper, or scissors. \n Player: ${playerScore} \n Computer: ${computerScore}`);
+let computerSelection = getComputerChoice()
+
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', registerClick)
+});
+
+let playerSelection;
+
+function registerClick() {
+    playerSelection = this.id;
+    playRound();
+}
+
+const results = document.getElementById('results')
+
+function declareResults() {
+    if (playerScore < 5 && computerScore < 5) {
+        results.textContent = `Player: ${playerScore} \n\n\n\nComputer: ${computerScore}`;
+    } else if (playerScore === 5) {
+        results.textContent = `Player has won 5 rounds! Player wins!`;
+    } else if (computerScore === 5) {
+        results.textContent = `Computer has won 5 rounds! Computer wins!`;
     }
 }
 
-function game() {
-    while ((playerScore < 3) && (computerScore < 3)) {
-            playRound()
-        }
-    winner()
+function playRound() {
+    let computerSelection = getComputerChoice();
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore += 1;
+        console.log(`Rock beats scissors! You win! \n Player: ${playerScore} \n Computer: ${computerScore}`);
+        declareResults();
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore += 1;
+        console.log(`Scissors beats paper! You win! \n Player: ${playerScore} \n Computer: ${computerScore}`);
+        declareResults();
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore += 1;
+        console.log(`Paper beats rock! You win! \n Player: ${playerScore} \n Computer: ${computerScore}`);
+        declareResults();
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore += 1;
+        console.log(`Paper beats rock! You lose! \n Player: ${playerScore} \n Computer: ${computerScore}`);
+        declareResults();
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore += 1;
+        console.log(`Scissors beats paper! You lose! \n Player: ${playerScore} \n Computer: ${computerScore}`);
+        declareResults();
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore += 1;
+        console.log(`Rock beats scissors! You lose! \n Player: ${playerScore} \n Computer: ${computerScore}`);
+        declareResults();
+    } else if (playerSelection === computerSelection) {
+        console.log(`You both picked ${playerSelection}! It's a draw!  \n Player: ${playerScore} \n Computer: ${computerScore}`);
+        results.textContent = `Player: ${playerScore} \n Computer: ${computerScore}`;
+    } else {
+        console.log(`Please choose rock, paper, or scissors. \n Player: ${playerScore} \n Computer: ${computerScore}`);
+        results.textContent = `Player: ${playerScore} \n Computer: ${computerScore}`;
+    }
 }
 
 function winner() {
